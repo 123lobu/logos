331f65d0d5075da11cd92a1623f90b8b0c95dcd4
Henock__Lobu <henocklobu12@gmail.com>
2025-05-30 08:56:18 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+
+const toggleBtn = document.querySelector('.toggle_btn')
+const toggleBtnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+   
+        toggleBtn.onclick = function () {
+                dropDownMenu.classList.toggle('open');
+                const isOpen = dropDownMenu.classList.contains('open');
+
+                toggleBtnIcon.className = isOpen ? 'bx bx-x' : 'bx bx-menu';        
+        };
+
+
+
+ 
